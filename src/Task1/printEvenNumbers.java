f292685ce4668d30ba07f7a03a91b86b93d05c96
stave031 <stevealba7382@gmail.com>
2022-08-27 22:19:56 -0400

    added even number class

---
+++ src/Task1/printEvenNumbers.java
@@ -0,0 +1,11 @@
+package Task1;
+
+public class printEvenNumbers {
+    public static void main(String[] args) {
+        for(int i=2; i<=100; i++){
+            if(i % 2==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
