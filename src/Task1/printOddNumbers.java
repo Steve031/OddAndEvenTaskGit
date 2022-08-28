245b4a692976cfa8c3e126b4d5ff50dee8cd7abf
stave031 <stevealba7382@gmail.com>
2022-08-27 21:58:37 -0400

    prints only odd numbers 1 to 100

---
+++ src/Task1/printOddNumbers.java
@@ -0,0 +1,11 @@
+package Task1;
+
+public class printOddNumbers {
+    public static void main(String[] args) {
+        for(int i=1; i<=99; i++){
+            if(i % 2!=0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
